90a279d5f14e198abb26376745b9245a8d5cccc5
Matthew Poole Chicano <matthew.poole485@gmail.com>
2025-12-10 20:35:59 -0500

    Added extracted dashboard module and formated code for readability.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,32 +1,43 @@
-  -- SNACKS.NVIM: The "Face"
-  -- RESOURCE: https://github.com/folke/snacks.nvim
+-- SNACKS.NVIM: The "Face"
+-- RESOURCE: https://github.com/folke/snacks.nvim
+local dashboard = require("snacks-dashboard")
 return {
   {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
     opts = {
-      dashboard = { enabled = true },
-      input = { enabled = true },
-      notifier = { enabled = true },
-      indent = { enabled = true },
-      scroll = { enabled = true },
-      words = { enabled = true },
-      statuscolumn = { enabled = true },
-      lazygit = { enabled = true },
-      terminal = { enabled = true },
-      picker = {
-        enabled = true,
-        sources = { explorer = { enabled = false } }, -- Handeled by mini.files
-      },
+      bigfile =             { enabled = true },
+      quickfile =           { enabled = true },
+      scroll =              { enabled = true },
+      statuscolumn =        { enabled = true },
+      terminal =            { enabled = true },
+      words =               { enabled = true },
+      indent =              { enabled = true },
+      input =               { enabled = true },
+      lazygit =             { enabled = true },
+      notifier =            { enabled = true },
+
+      dashboard =           { enabled = true,
+        preset =            { header = dashboard.header },
+        sections =            dashboard.sections, },
+
+      picker =              { enabled = true,
+        sources =
+        { explorer =        { enabled = false } } },
     },
+
+    -- ========================================================================
+    -- keymaps
+    -- ------------------------------------------------------------------------
     keys = {
-      { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find" },
-      { "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
-      { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
-      { "<leader>:", function() Snacks.picker.command_history() end, desc = "Cmd History" },
-      { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
-      { "<c-/>", function() Snacks.terminal() end, desc = "Toggle Terminal" },
+      { "<leader><space>",  function() Snacks.picker.smart() end,           desc = "Smart Find" },
+      { "<leader>.",        function() Snacks.scratch() end,                desc = "Scratch Buffer" },
+      { "<leader>,",        function() Snacks.picker.buffers() end,         desc = "Buffers" },
+      { "<leader>/",        function() Snacks.picker.grep() end,            desc = "Grep" },
+      { "<leader>:",        function() Snacks.picker.command_history() end, desc = "Cmd History" },
+      { "<leader>gg",       function() Snacks.lazygit() end,                desc = "Lazygit" },
+      { "<c-/>",            function() Snacks.terminal() end,               desc = "Terminal" },
     },
   },
 }
