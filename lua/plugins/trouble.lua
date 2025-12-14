189e4cdc63273aace54304503568315c156ee326
Matthew Poole Chicano <matthew.poole485@gmail.com>
2025-12-14 00:51:49 -0500

    docs(trouble): add reference link and inline comments

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,50 +1,67 @@
+-- =============================================================================
+-- TROUBLE.NVIM
+-- WHAT: Pretty panel for diagnostics, symbols, quickfix, and location lists
+-- REFERENCE: https://github.com/folke/trouble.nvim
+-- -----------------------------------------------------------------------------
 return {
-  {
-    "folke/trouble.nvim",
-    lazy = true,
-    cmd = { "Trouble" },
-    event = { "BufRead", "BufNewFile", "VeryLazy" },
-    opts = {
-      modes = {
-        lsp = {
-          win = { position = "right" },
-        },
+  'folke/trouble.nvim',
+  lazy = true,
+  cmd = { 'Trouble' },
+  event = { 'BufRead', 'BufNewFile', 'VeryLazy' },
+  opts = {
+    modes = {
+      lsp = {
+        win = { position = 'right' },  -- LSP symbols appear on right side
       },
     },
-    keys = {
-      { "<leader>xd", "<cmd>Trouble diagnostics toggle<cr>", desc = "[D]iagnostics" },
-      { "<leader>xD", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer [D]iagnostics" },
-      { "<leader>xs", "<cmd>Trouble symbols toggle<cr>", desc = "[S]ymbols" },
-      { "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "[L]ocation List" },
-      { "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "[Q]uickfix List" },
-      {
-        "[q",
-        function()
-          if require("trouble").is_open() then
-            require("trouble").prev({ skip_groups = true, jump = true })
-          else
-            local ok, err = pcall(vim.cmd.cprev)
-            if not ok then
-              vim.notify(err, vim.log.levels.ERROR)
-            end
-          end
-        end,
-        desc = "Prev Quickfix Item",
-      },
-      {
-        "]q",
-        function()
-          if require("trouble").is_open() then
-            require("trouble").next({ skip_groups = true, jump = true })
-          else
-            local ok, err = pcall(vim.cmd.cnext)
-            if not ok then
-              vim.notify(err, vim.log.levels.ERROR)
-            end
-          end
-        end,
-        desc = "Next Quickfix Item",
-      },
+  },
+  keys = {
+    {
+      '<leader>xd',
+      '<cmd>Trouble diagnostics toggle<cr>',
+      desc = '[D]iagnostics',
     },
-  }
+    {
+      '<leader>xD',
+      '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+      desc = 'Buffer [D]iagnostics',
+    },
+    { '<leader>xs', '<cmd>Trouble symbols toggle<cr>', desc = '[S]ymbols' },
+    {
+      '<leader>xl',
+      '<cmd>Trouble loclist toggle<cr>',
+      desc = '[L]ocation List',
+    },
+    {
+      '<leader>xq',
+      '<cmd>Trouble qflist toggle<cr>',
+      desc = '[Q]uickfix List',
+    },
+    {
+      '[q',
+      function()
+        -- If Trouble is open, use Trouble navigation; otherwise use quickfix
+        if require('trouble').is_open() then
+          require('trouble').prev({ skip_groups = true, jump = true })
+        else
+          local ok, err = pcall(vim.cmd.cprev)
+          if not ok then vim.notify(err, vim.log.levels.ERROR) end
+        end
+      end,
+      desc = 'Prev [Q]uickfix Item',
+    },
+    {
+      ']q',
+      function()
+        -- If Trouble is open, use Trouble navigation; otherwise use quickfix
+        if require('trouble').is_open() then
+          require('trouble').next({ skip_groups = true, jump = true })
+        else
+          local ok, err = pcall(vim.cmd.cnext)
+          if not ok then vim.notify(err, vim.log.levels.ERROR) end
+        end
+      end,
+      desc = 'Next [Q]uickfix Item',
+    },
+  },
 }
