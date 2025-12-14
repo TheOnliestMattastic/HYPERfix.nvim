faaf06fd8358a9f2461a6506f08777d864c1d89f
Matthew Poole Chicano <matthew.poole485@gmail.com>
2025-12-14 00:52:16 -0500

    feat(lazydev): extract lazydev into separate plugin file

---
+++ lua/plugins/lazydev.lua
@@ -0,0 +1,17 @@
+  -- ===========================================================================
+  -- LAZYDEV.NVIM: Lua Development Support
+  -- https://github.com/folke/lazydev.nvim
+  -- WHAT: Provides Lua type hints for nvim/luv/plugin libraries
+  -- WHY:  Enables completion and type checking in Neovim config files
+  -- ---------------------------------------------------------------------------
+return  {
+  'folke/lazydev.nvim',
+  lazy = true,
+  ft = 'lua',
+  opts = {
+    library = {
+      { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+      { path = 'snacks.nvim', words = { 'Snacks' } },
+    }
+  }
+}
