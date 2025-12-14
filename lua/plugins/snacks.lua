9f21ec9002799a2b328f5a64c6db51eef41d9f54
Matthew Poole Chicano <matthew.poole485@gmail.com>
2025-12-14 00:52:11 -0500

    refactor(snacks): reorganize and add section comments

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,59 +4,63 @@
 -- -----------------------------------------------------------------------------
 local dashboard = require("snacks-dashboard")
 return {
-  {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    opts = {
-      animate =             { enabled = true },
-      bigfile =             { enabled = true },
-      bufdelete =           { enabled = true },
-      debug =               { enabled = true },
-      dim =               { enabled = true },
-      image =               { enabled = true },
-      indent =              { enabled = true },
-      input =               { enabled = true },
-      lazygit =             { enabled = true },
-      notifier =            { enabled = true },
-      quickfile =           { enabled = true },
-      rename =              { enabled = true },
-      scope =               { enabled = true },
-      scratch =             { enabled = true },
-      scroll =              { enabled = true },
-      statuscolumn =        { enabled = true },
-      terminal =            { enabled = true },
-      toggle =              { enabled = true },
-      util =                { enabled = true },
-      words =               { enabled = true },
-      zen =                 { enabled = true },
-
-      -- =======================================================================
-      -- dashboard
-      -- -----------------------------------------------------------------------
-      dashboard =           { enabled = true,
-        preset =            { header = dashboard.header },
-        sections =            dashboard.sections, },
-
-      -- =======================================================================
-      -- picker
-      -- -----------------------------------------------------------------------
-      picker =              { enabled = true,
-        sources =
-        { explorer =        { enabled = false } } },
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    -- =======================================================================
+    -- dashboard
+    -- -----------------------------------------------------------------------
+    dashboard =           { enabled = true,
+      preset =            { header = dashboard.header },
+      sections =            dashboard.sections,
     },
 
-    -- =========================================================================
-    -- keymaps
-    -- -------------------------------------------------------------------------
-    keys = {
-      { "<leader><space>",  function() Snacks.picker.smart() end,           desc = "Smart Find" },
-      { "<leader>.",        function() Snacks.scratch() end,                desc = "Scratch Buffer" },
-      { "<leader>,",        function() Snacks.picker.buffers() end,         desc = "Buffers" },
-      { "<leader>/",        function() Snacks.picker.grep() end,            desc = "Grep" },
-      { "<leader>:",        function() Snacks.picker.command_history() end, desc = "Cmd History" },
-      { "<leader>gg",       function() Snacks.lazygit() end,                desc = "Lazygit" },
-      { "<c-/>",            function() Snacks.terminal() end,               desc = "Terminal" },
+    -- =======================================================================
+    -- picker
+    -- -----------------------------------------------------------------------
+    picker =              { enabled = true,
+      sources =
+      { explorer =        { enabled = false } }
     },
+
+    animate =             { enabled = true },
+    bigfile =             { enabled = true },
+    bufdelete =           { enabled = true },
+    debug =               { enabled = true },
+    dim =                 { enabled = true },
+    image =               { enabled = true },
+    indent =              { enabled = true },
+    input =               { enabled = true },
+    layout =              { enabled = true },
+    lazygit =             { enabled = true },
+    notifier =            { enabled = true },
+    notify =              { enabled = true },
+    profiler =            { enabled = true },
+    quickfile =           { enabled = true },
+    rename =              { enabled = true },
+    scope =               { enabled = true },
+    scratch =             { enabled = true },
+    scroll =              { enabled = true },
+    statuscolumn =        { enabled = true },
+    terminal =            { enabled = true },
+    toggle =              { enabled = true },
+    util =                { enabled = true },
+    words =               { enabled = true },
+    zen =                 { enabled = true },
+  },
+
+
+  -- =========================================================================
+  -- keymaps
+  -- -------------------------------------------------------------------------
+  keys = {
+    { "<leader><space>",  function() Snacks.picker.smart() end,           desc = "Smart Find" },
+    { "<leader>.",        function() Snacks.scratch() end,                desc = "Scratch Buffer" },
+    { "<leader>,",        function() Snacks.picker.buffers() end,         desc = "Buffers" },
+    { "<leader>/",        function() Snacks.picker.grep() end,            desc = "Grep" },
+    { "<leader>:",        function() Snacks.picker.command_history() end, desc = "Cmd History" },
+    { "<leader>gg",       function() Snacks.lazygit() end,                desc = "Lazygit" },
+    { "<c-/>",            function() Snacks.terminal() end,               desc = "Terminal" },
   },
 }
