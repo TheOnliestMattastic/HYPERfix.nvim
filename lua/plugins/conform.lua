bc4b2a935cb46c21cb275f5db3890bcb787a1544
Matthew Poole Chicano <matthew.poole485@gmail.com>
2025-12-14 00:52:18 -0500

    feat(conform): extract conform into separate plugin file

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,21 @@
+return {
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>cf',
+      function()
+        require('conform').format({ async = true, lsp_format = 'fallback' })
+      end,
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+  },
+  opts = {
+    notify_on_error = true,
+    formatters_by_ft = {
+      lua = { 'stylua' },
+    },
+  },
+}
