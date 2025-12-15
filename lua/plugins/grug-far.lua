62813c02c2d5f36f529093f7d4f15aea18a524d9
Matthew Poole Chicano <matthew.poole485@gmail.com>
2025-12-15 02:07:56 -0500

    refactor(grug-far): normalize quotes, simplify events, fix keymap from sr to sR

--- lua/plugins/grug-far.lua
+++ lua/plugins/grug-far.lua
@@ -4,27 +4,27 @@
 -- -----------------------------------------------------------------------------
 return {
   {
-    "MagicDuck/grug-far.nvim",
+    'MagicDuck/grug-far.nvim',
     lazy = true,
-    event = { 'BufRead', 'BufNewFile', 'BufEnter', 'VeryLazy' },
+    event = { 'BufRead', 'VeryLazy' },
     opts = { headerMaxWidth = 80 },
-    cmd = { "GrugFar", "GrugFarWithin" },
+    cmd = { 'GrugFar', 'GrugFarWithin' },
     keys = {
       {
-        "<leader>sr",
+        '<leader>sR',
         function()
-          local grug = require("grug-far")
-          local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+          local grug = require('grug-far')
+          local ext = vim.bo.buftype == '' and vim.fn.expand('%:e')
           grug.open({
             transient = true,
             prefills = {
-              filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+              filesFilter = ext and ext ~= '' and '*.' .. ext or nil,
             },
           })
         end,
-        mode = { "n", "x" },
-        desc = "Search and Replace",
+        mode = { 'n', 'x' },
+        desc = 'Search and [R]eplace',
       },
     },
-  }
+  },
 }
