bce1d46e15e886edcef623a2c28613a9757dbeea
Matthew Poole Chicano <matthew.poole485@gmail.com>
2025-12-15 02:08:00 -0500

    docs(mason): add comprehensive usage guide and update keymap/icons

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,15 +1,26 @@
+-- =============================================================================
+-- MASON.NVIM - Package Manager for Language Servers
+-- =============================================================================
+-- WHAT: Install and manage language servers, formatters, and linters
+-- WHY:  LSP servers and tools must be installed separately from Neovim.
+--       Mason provides a unified UI to install/update them without terminal.
+-- HOW:  Press '<leader>nm' to open Mason. Browse packages, press 'i' to install.
+--       Installed tools are automatically used by LSP, conform, and other plugins.
+-- NOTE: Works with nvim-lspconfig to automatically attach servers to buffers.
+--
+-- REFERENCE: https://github.com/williamboman/mason.nvim
+-- -----------------------------------------------------------------------------
 return {
   "mason-org/mason.nvim",
   lazy = true,
   event = { 'BufRead', 'BufNewFile', 'BufEnter', 'VeryLazy' },
-  keys = { { '<leader>cm', '<cmd>Mason<cr>', desc = 'Mason' } },
+  keys = { { '<leader>nm', '<cmd>Mason<cr>', desc = '[M]ason' } },
   opts = {
     ui = {
-      -- TODO: change icons
       icons = {
-        package_installed = "✓",
-        package_pending = "➜",
-        package_uninstalled = "✗"
+        package_installed = "󰳈",
+        package_pending = "󰇠",
+        package_uninstalled = "󰞇"
       }
     }
   }
